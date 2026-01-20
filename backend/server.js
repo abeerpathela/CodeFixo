8d2fc73d0c501d5785fd3134139364df18bfecf1
abeerpathela <pathelaabeer@gmail.com>
2026-01-20 23:13:15 +0530

    changed the ports

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
+app.set('trust proxy', 1);
 
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
